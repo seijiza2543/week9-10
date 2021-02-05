4a9ffb5402990cbb1bbaff9468750c856b33e340
seijiza2543 <honeyly2536@gmail.com>
2021-02-05 13:30:50 +0700

    Deploy to Github page

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const AboutScreen = () => {
+    return (
+        <div className="container mt-3">
+<div className="container">
+<h2 className="text-success">เกี่ยวกับ</h2>
+<p>
+เริ่มก่อตั้งเมื่อ ปี2480 ใช้ชื่อ“โรงเรียนช่างไม้” ในเนื ้อที่ของกรมการศาสนาวัดบุรินทร์(วัดร้าง) ประมาณ 6ไร่เศษ โดยมีนายจิรภัทร สถาการ เป็น ครูใหญ่ ในสมัยนั้น ปีพุทธศักราช2510
+โดยการใช้พื้นที่ของโรงเรียนการช่างสตรีซึ่งถูกยุบรวมกับ โรงเรียนช่างไม้ในเนื ้อที่15 ไร่1งาน 72 ตารางวาอันเป็นที่ ตั้งของวิทยาลัยเทคนิคในปัจจุบัน
+</p>
+</div>
+<div className="container">
+<ul type="square">
+<li>ปีพ.ศ.2480 โรงเรียนช่างไม้ตราด</li>
+<li>ปีพ.ศ.2500 โรงเรียนการช่างตราด</li>
+<li>ปีพ.ศ.2510 รวมกับ โรงเรียนการช่างสตรีใช้ชื่อโรงเรียนการช่างตราด</li>
+<li>ปีพ.ศ.2522 โรงเรียนเทคนิคตราด</li>
+<li>ปีพ.ศ.2525 ถึง ปัจจุบัน วิทยาลัยเทคนิคตราด</li>
+</ul>
+</div>
+<div className="container ml-5">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_u7C_usM2I3XohEL6qPYTK7HyjahlXiXNsg&usqp=CAU" width="400px" height="300px"/>
+</div>
+</div>
+    )
+}
+
+export default AboutScreen
